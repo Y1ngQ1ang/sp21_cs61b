affb632d1552ace045add9e3904cf3e533e48889
y1ngq1ang <wyq123445@qq.com>
2023-10-04 21:29:57 +0800

    Finish proj0

--- proj0/game2048/Board.java
+++ proj0/game2048/Board.java
@@ -83,6 +83,9 @@ public class Board implements Iterable<Tile> {
     /** Places the Tile TILE at column COL, row ROW where COL and ROW are
      * treated as coordinates with respect to the current viewPerspective.
      *
+     * @param col destination col
+     * @param row destination row
+     * @param tile source
      * Returns whether or not this move is a merge.
      * */
     public boolean move(int col, int row, Tile tile) {
